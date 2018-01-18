0fd6cdd1677b6b29a9b47bbebcda16264c038219
Shawnk <sdf1342@gmail.com>
2018-01-18 11:23:00 -0800

    Sec03_Lec65

--- BuildingEscape/Source/BuildingEscape/Private/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/Private/PositionReport.cpp
@@ -1,14 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-#include "Gameframework/Actor.h"
+#include "Runtime/Engine/Classes/Gameframework/Actor.h"
+
+// **IWYU Reference
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
@@ -21,8 +22,11 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	FString ObjectPosX = FString::SanitizeFloat(GetOwner()->GetTransform().GetLocation().X);
+	FString ObjectPosY = FString::SanitizeFloat(GetOwner()->GetTransform().GetLocation().Y);
 
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s"), *ObjectName);
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s (%s, %s)"), *ObjectName, *ObjectPos, *ObjectPosX, *ObjectPosY);
 }
 
 
